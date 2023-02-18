664fd760fd0f0d99ea9fd79452dcef74e233f6d7
Sanay Singh Rajawat <sanaysinghvlogs@gmail.com>
2023-02-18 17:37:09 +0530

    Update Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -1,56 +1,53 @@
-from turtle import*
+from turtle import *
 from random import randrange
-from freegames import square,vector
+from freegames import square, vector
 
-food=vector(0,0)
-snake=[vector(10,0)]
-aim=vector(0,-10)
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
 
-def change(x,y):
-    aim.x=x
-    aim.y=y
+def change(x, y):
+    aim.x = x
+    aim.y = y
 
 def inside(head):
-    return -200 < head.x <190 and -200
+    return -200 < head.x < 190 and -200 < head.y < 190
 
 def move():
-    head=snake[-1].copy()
+    head = snake[-1].copy()
     head.move(aim)
 
     if not inside(head) or head in snake:
-        square(head.x,head.y,9,'red')
+        square(head.x, head.y, 9, 'red')
         update()
         return
 
-    snake.append()
+    snake.append(head)
 
-    if head==food:
-        print('snake',len(snake))
-        food.x=randrange(-15,15)*10
-        food.y=randrange(-15,15)*10
-
-
+    if head == food:
+        print('snake', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
     else:
         snake.pop(0)
 
     clear()
 
     for body in snake:
-        square(body.x,body.y,9,'green')
+        square(body.x, body.y, 9, 'green')
 
-
-    square(food.x,food.y,9,'red')
+    square(food.x, food.y, 9, 'red')
     update()
     ontimer(move, 100)
 
-    hideturtle()
-    tracer(False)
-    listen()
-    onkey(lambda:change(10,0),'Right')
-    onkey(lambda:change(-10,0),'Left')
-    onkey(lambda:change(10,0),'Up')
-    onkey(lambda:change(0,-10),'Down')
+# initialize the game
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, -10), 'Down')
+onkey(lambda: change(0, 10), 'Up')
 
-    move()
-    done()
-    
+move()
+done()
